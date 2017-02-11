d7417baee91d4f3f5181f92599b763e591868168
[trAns__EL Capitian] <trans__elcapitian@air.local>
2017-02-12 00:30:37 +0530

    First commit

---
+++ code.js
@@ -0,0 +1,50 @@
+function changeToGreen()
+{
+	document.getElementById("h2color").style.color="green";
+}
+
+function sayHello()
+{
+	alert("Hello");
+}
+
+function doYouWantToContinue()
+{
+	var confirmation=confirm("Do you want to continue?");
+	if(confirmation==true)
+	{
+		alert("You clicked OK!");
+	}
+	else
+	{
+		alert("You clicked on cancel");
+	}
+}
+
+function acceptInput()
+{
+	var x=prompt("Please Enter a number");
+	alert("You entered "+x);
+}
+
+function addition()
+{
+	var x=prompt("Please enter first number");
+	var y=prompt("Please enter second number");
+	x=x-0;
+	y=y-0;	
+	var z=x+y;
+	alert("Sum= "+z);
+}
+
+function array()
+{
+	var list=new Array(10,20,30);
+	var i=0,sum=0;
+	for(i=0;i<3;i++)
+	{
+
+		sum+=list[i];
+	}
+	alert("Sum="+sum);
+}
---
+++ test.js
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Webpage</title>
+<script>
+function changeToYellow()
+{
+	document.getElementById("h2color").style.color="yellow";
+}
+function changeToBlack()
+{
+	document.getElementById("h2color").style.color="black";
+}
+
+</script>
+<script type="text/javascript" src="code.js">
+</script></head>
+<body>
+<h1 onmouseover="this.style.color='yellow';" onmouseout="this.style.color='blue';">This is example code for Javascript</h1>
+<p>Sample Text</p>
+<button onmouseover="this.style.color='yellow';"onmouseout="this.style.color='blue';" onclick="sayHello();">Move me!</button>
+<h2 id="h2color" onmouseover="changeToGreen();" onmouseout="changeToBlack();">Using functions colour change</h2>
+<button onclick="doYouWantToContinue();">Continue?</button>
+<hr>
+<button onclick="acceptInput();">Enter Number?</button>
+<hr>
+<button onclick="addition();">Adding two numbers?</button>
+<hr>
+<button onclick="array();">Array addition?</button>
+</body>
+</html>
